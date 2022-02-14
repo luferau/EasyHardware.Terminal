05d9eebfd3ba1792f05dd3492d831cb532c72890
Aliaksei Luferau <aliaksei.luferau@gmail.com>
2022-02-14 15:21:02 +0300

    MAUI app created

---
+++ source/EasyHardware.Terminal/EasyHardware.Terminal/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EasyHardware.Terminal;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
